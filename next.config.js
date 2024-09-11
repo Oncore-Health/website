77c3fb225f3489d90100249a5545e69d08065f2b
Sruthi Kurada <sruthi.kurada1@gmail.com>
2024-09-11 00:11:49 -0400

    made changes in next config file because we are using a custom domain

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  basePath: isProd ? "/website" : "",  // Only add basePath in production
-  assetPrefix: isProd ? "/website/" : "",  // Only add assetPrefix in production
   output: "export",  // Static export mode for GitHub Pages
   reactStrictMode: true,
   images: {
