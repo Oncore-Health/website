bdc6620c2353b2863eb714b876606f7954d5d0f4
Sruthi Kurada <sruthi.kurada1@gmail.com>
2025-05-19 15:23:32 -0400

    removed github pages related files

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: "export",  // Static export mode for GitHub Pages
   reactStrictMode: true,
   images: {
-    unoptimized: true,  // Disable Next.js Image Optimization for static export
     domains: ['media.licdn.com'], // Allow LinkedIn media domain
   },
 };
